e7b60f3a0f80bb8bc83e46671638c258e2589cf9
faris <faris38kanbur@gmail.com>
2021-04-23 22:28:14 +0200

    add custom css

--- material-ui-tut/src/pages/Create.js
+++ material-ui-tut/src/pages/Create.js
@@ -1,12 +1,52 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import SendIcon from '@material-ui/icons/SendOutlined';
+import KeyboardArrowRightOutlinedIcon from '@material-ui/icons/KeyboardArrowRightOutlined';
+import {makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  btn:{
+    fontSize: 60,
+    backgroundColor: 'violet', 
+    '&:hover':{
+      backgroundColor: 'blue'
+    }
+  },
+  title:{
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+})
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Typography variant="h6" color="textSecondary" component="h2" gutterBottom>
+    <Container>
+      <Typography 
+      className={classes.title}
+      variant="h6" 
+      color="textSecondary" 
+      component="h2" 
+      gutterBottom>
         Create a New Note
       </Typography>
-    </div>
+
+      <Button 
+      className={classes.btn}
+      onClick={() => console.log("You click me")}
+      type="submit" 
+      color="secondary" 
+      variant="contained"
+      // startIcon={<SendIcon />}
+      endIcon={<KeyboardArrowRightOutlinedIcon />}
+      >
+      Submit
+      </Button>
+      <br/>
+      
+    </Container>
   )
 }
