fe705a208dcb1f90db52207b6a09e932a401b9a8
faris <faris38kanbur@gmail.com>
2021-04-25 00:46:11 +0200

    add new data and JSON.stringify

--- material-ui-tut/src/pages/Create.js
+++ material-ui-tut/src/pages/Create.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -8,8 +8,9 @@ import Container from '@material-ui/core/Container';
 import SendIcon from '@material-ui/icons/SendOutlined';
 import KeyboardArrowRightOutlinedIcon from '@material-ui/icons/KeyboardArrowRightOutlined';
 import {FormControlLabel, makeStyles} from '@material-ui/core';
-import FormControl from '@material-ui/core/FormControl'
-import FormLabel from '@material-ui/core/FormLabel'
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import {useHistory} from 'react-router-dom';
 
 const useStyles = makeStyles({
   filed:{
@@ -21,6 +22,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
   const [titleError, setTitleError] = useState(false)
@@ -41,7 +43,11 @@ export default function Create() {
     };
 
     if(title && details) {
-      console.log(title, details, category)
+      fetch('http://localhost:8000/notes' , {
+        method: 'POST',
+        headers: {"Content-type" : "application/json"},
+        body: JSON.stringify({title, details, category })
+      }).then(() => history.push('/'))
     }
   }
 
